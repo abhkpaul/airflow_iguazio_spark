2f16020e3310da4b85171dcdbaff0defa51dee1f
abhkpaul <abhk.paul@gmail.com>
2021-12-21 08:03:54 -0500

    Initial Commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Create an egg file of the project
 '''
 from setuptools import setup, find_packages
 
-setup(name="igz_func",
+setup(name="airflow_iguazio_spark",
       version="1.0",
       packages=find_packages(),
       install_requires=[
